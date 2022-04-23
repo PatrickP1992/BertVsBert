e883bf5ef80b641b0fb04f4482a9535ce98f5d99
PatrickP1992 <42120798+PatrickP1992@users.noreply.github.com>
2022-04-23 17:06:55 +0200

    Darkness

--- Assets/Scripts/DarknessItem.cs
+++ Assets/Scripts/DarknessItem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DarknessItem : MonoBehaviour
 {
+    public GameObject blackScreenLeft;
+    public GameObject blackScreenRight;
     // Start is called before the first frame update
     void Start()
     {
