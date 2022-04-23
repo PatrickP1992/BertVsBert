ad14ffdf886c93ddf485da2cd319c66ddd615f6d
PatrickP1992 <42120798+PatrickP1992@users.noreply.github.com>
2022-04-23 18:33:56 +0200

    commit

--- Assets/Scripts/DarknessItem.cs
+++ Assets/Scripts/DarknessItem.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DarknessItem : MonoBehaviour
 {
     public GameObject blackScreenLeft;
     public GameObject blackScreenRight;
+    public BoxCollider2D col;
+    public SpriteRenderer spriteRen;
+    public Rigidbody2D rBody;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +21,40 @@ public class DarknessItem : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player1"))
+        {
+            blackScreenLeft.SetActive(true);
+            StartCoroutine(waitTime(3));
+            spriteRen.enabled = false;
+            col.enabled = false;
+            StartCoroutine(waitTime2(5));
+        }
+        else if (other.tag.Equals("Player2"))
+        {
+            blackScreenRight.SetActive(true);
+            StartCoroutine(waitTime(3));
+            spriteRen.enabled = false;
+            col.enabled = false;
+            StartCoroutine(waitTime2(5));
+        }
+    }
+
+    IEnumerator waitTime(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        blackScreenRight.SetActive(false);
+        blackScreenLeft.SetActive(false);
+        
+    }
+    IEnumerator waitTime2(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        
+        spriteRen.enabled = true;
+        col.enabled = true;
     }
 }
